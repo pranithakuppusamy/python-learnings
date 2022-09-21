131254d615b6fe430c5d38c9e6175747acc95114
Pranitha K <pranithakuppusamy02@gmail.com>
2022-09-21 22:04:13 +0530

    initial commit text

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print("hello world")
+if 1 == 1:
+    print("yes")
+print("no")
---
+++ variable.py
@@ -0,0 +1,11 @@
+
+
+# x = 10
+# y = 20
+# print(y)
+x="hello"
+y=x
+z="john"
+Z="John"
+print("x = ",x,"X =","Y =","y = ",y,"Z = ", Z , "z =", z)
+
